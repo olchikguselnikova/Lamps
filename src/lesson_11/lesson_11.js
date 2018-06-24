3d5f937a012a2be7de4fd36ac6d9c468c97f79af
Olga Guselnikova <olchikguselnikova@gmail.com>
2018-06-25 01:17:28 +0300

    Fixing script to make it work

--- src/lesson_11/lesson_11.js
+++ src/lesson_11/lesson_11.js
@@ -1,20 +1,18 @@
 import './lesson_11.scss';
 
-let titleItem = document.querySelectorAll('.panel-group__title'),
-bodyItem = document.querySelectorAll('.panel-group__body');
+let acc = document.getElementsByClassName("panel-group__title");
+let i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+       
+        this.classList.toggle("active");
 
-titleItem.forEach(function(item, i, titleItem){
-item.addEventListener('click', function(e){
-if (this.classList.contains('panel-active')) {
-    bodyItem[i].classList.remove('active');
-    this.classList.remove('panel-active');
-} else {
-    item[i].classList.remove('panel-active');
-    titleItem[i].classList.remove('panel-active');
-    bodyItem[i].classList.remove('active');
-    this.classList.add('panel-active');
-    this.nextElementSibling.classList.add('active');
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
-});
-});
